d2db6ce30072fe346514e50237ad21e03511135f
breakthrough23 <108655207+breakthrough23@users.noreply.github.com>
2023-12-01 12:45:13 +0100

    first commit

---
+++ src/data.js
@@ -0,0 +1,102 @@
+export const shortList = [
+  {
+    id: 1,
+    image: "https://www.course-api.com/images/people/person-1.jpeg",
+    name: "maria ferguson",
+    title: "office manager",
+    quote: "Unshakable faith shakes up everything!",
+  },
+];
+
+export const list = [
+  {
+    id: 1,
+    image: "https://www.course-api.com/images/people/person-1.jpeg",
+    name: "maria ferguson",
+    title: "office manager",
+    quote: "Unshakable faith shakes up everything!",
+  },
+  {
+    id: 2,
+    image: "https://www.course-api.com/images/people/person-4.jpeg",
+    name: "john doe",
+    title: "regular guy",
+    quote: "Once you choose hope, anythings possible..",
+  },
+  {
+    id: 3,
+    image: "https://www.course-api.com/images/people/person-3.jpeg",
+    name: "peter smith",
+    title: "product designer",
+    quote:
+      "Her beauty is laced in her strength and interwoven through her flaws. She embodies perfection",
+  },
+  {
+    id: 4,
+    image: "https://www.course-api.com/images/people/person-2.jpeg",
+    name: "susan andersen",
+    title: "the boss",
+    quote: "Once you choose love, you are at the end",
+  },
+];
+
+export const longList = [
+  {
+    id: 1,
+    image: "https://www.course-api.com/images/people/person-1.jpeg",
+    name: "maria ferguson",
+    title: "office manager",
+    quote: "Unshakable faith shakes up everything!",
+  },
+  {
+    id: 2,
+    image: "https://www.course-api.com/images/people/person-4.jpeg",
+    name: "john doe",
+    title: "regular guy",
+    quote: "Once you choose hope, anythings possible..",
+  },
+  {
+    id: 3,
+    image: "https://www.course-api.com/images/people/person-3.jpeg",
+    name: "peter smith",
+    title: "product designer",
+    quote:
+      "Her beauty is laced in her strength and interwoven through her flaws. She embodies perfection",
+  },
+  {
+    id: 4,
+    image: "https://www.course-api.com/images/people/person-2.jpeg",
+    name: "susan andersen",
+    title: "the boss",
+    quote: "Once you choose love, you are at the end ",
+  },
+  {
+    id: 5,
+    image: "https://www.course-api.com/images/people/person-1.jpeg",
+    name: "maria ferguson",
+    title: "office manager",
+    quote: "Unshakable faith shakes up everything!",
+  },
+  {
+    id: 6,
+    image: "https://www.course-api.com/images/people/person-4.jpeg",
+    name: "john doe",
+    title: "regular guy",
+    quote: "Once you choose hope, anythings possible..",
+  },
+  {
+    id: 7,
+    image: "https://www.course-api.com/images/people/person-3.jpeg",
+    name: "peter smith",
+    title: "product designer",
+    quote:
+      "Her beauty is laced in her strength and interwoven through her flaws. She embodies perfection",
+  },
+  {
+    id: 8,
+    image: "https://www.course-api.com/images/people/person-2.jpeg",
+    name: "susan andersen",
+    title: "the boss",
+    quote: "Once you choose love, you are at the end ",
+  },
+];
